9de708c5106f663a9a6ef1cfc937269191aa1dec
Alexander Drager <szdrager@gmail.com>
2022-03-26 14:44:33 +0700

    Minor linter fixes

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ ${result.map((e) => e.join(' ')).join('\n')}
 const genDiff = (filePath1, filePath2) => {
   const fileContent1 = path.extname(filePath1) === '.json' ? getDataFromJson(filePath1) : getDataFromYml(filePath1);
   const fileContent2 = path.extname(filePath2) === '.json' ? getDataFromJson(filePath2) : getDataFromYml(filePath2);
-  return compareObjects(fileContent1, fileContent2); 
+  return compareObjects(fileContent1, fileContent2);
 };
 
 export { compareObjects };
